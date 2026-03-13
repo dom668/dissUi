36e5984f8128fbf3b026d146d1df91ae6caf0dc2
dominic668jb <63196459+dom668@users.noreply.github.com>
2026-03-13 18:23:59 +0000

    started

--- ui.py
+++ ui.py
@@ -1,4 +1,3 @@
-# this is the ui
-
-
-# this really is the ui for realzzz
+### stuff to make it work fr
+# import subprocess
+# subprocess.run(["/usr/share/ofono/scripts/dial-number", "+447305874435"]) - this places a call when run on the pi :)
